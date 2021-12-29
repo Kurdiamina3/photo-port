565e808a8d51ae4639a859da3d3d31181adb7da9
Kurdiamina3 <kurdiamina3@gmail.com>
2021-12-29 13:58:12 -0500

    gallery and contact markup

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
+
 afterEach(cleanup);
+
 describe('About component', () => {
     // First Test
     it('renders', () => {
@@ -11,8 +13,12 @@ describe('About component', () => {
       });    
   // Second Test
   it('matches snapshot DOM node structure', () => {
-    const { asFragment } = render(<About />);expect(asFragment()).toMatchSnapshot();
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
   });
   
   })
+  
+  
   
